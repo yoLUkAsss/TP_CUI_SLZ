849e3f151eecedb808981881609a2c091b99e8d2
Ramshell <ramshellcinox@gmail.com>
2015-10-20 13:20:44 -0300

    refactor en carpetas

--- duelo_clienteLiviano/src/js/app.js
+++ duelo_clienteLiviano/src/js/app.js
@@ -1,7 +1,1 @@
-angular
-  .module('LobbyApp', [])
-  .controller('LobbyController',function($scope){
-    $scope.changeImgLobby = function (personaje){
-      document.getElementById("selectedCharacter").src = "css/images/Lobby/" + personaje + "Big.jpg";
-    }
-  })
+var appAng = angular.module('LobbyApp', [])
