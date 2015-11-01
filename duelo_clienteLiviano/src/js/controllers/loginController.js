2ce615687aa340e74396927152aa3d33010f73b8
Ramshell <ramshellcinox@gmail.com>
2015-10-31 23:16:59 -0300

    login en funcionamiento

--- duelo_clienteLiviano/src/js/controllers/loginController.js
+++ duelo_clienteLiviano/src/js/controllers/loginController.js
@@ -1,20 +1,8 @@
-// angular.module('dueloLeyendasApp')
-//       .controller('LoginController',function($scope,$state,LoginService,LobbyService){
-//   $scope.login = function (){
-//     var idPersonaje = document.getElementById("input").value;
-//     var contrasenia = document.getElementById("password").value;
-//     var res = LoginService.login(idPersonaje,contrasenia);
-//     $state.go('noTienesRival');
-//     if(res.id){
-//       LobbyService.datosDelJuego(res.id);
-//       return res.id;
-//     }
-//     return res.descripcion;
-//   }
-// })
+
 angular.module('dueloLeyendasApp')
       .controller('LoginController',['$scope','$state','LoginService','LobbyService',function($scope,$state,LoginService,LobbyService){
   $scope.user = {usuario: "", password: ""};
+  $scope.loginFailure=false;
   $scope.login = function (){
     LoginService.login($scope.user,$scope.callback,$scope.errorHandler);
   };
@@ -23,6 +11,6 @@ angular.module('dueloLeyendasApp')
   };
   $scope.errorHandler=function(error){
     alert(error.descripcion);
-
+    $scope.loginFailure=true;
   };
 }])
