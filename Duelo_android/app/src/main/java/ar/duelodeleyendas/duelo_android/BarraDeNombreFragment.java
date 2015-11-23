15f0786542c6b3793a2f4304419a0e4894f45e09
ExiLio <sandoval.lucasj@gmail.com>
2015-11-23 14:36:06 -0300

    Arrancando activity y fragment para la estadistica del personaje

---
+++ Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/BarraDeNombreFragment.java
@@ -0,0 +1,24 @@
+package ar.duelodeleyendas.duelo_android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by exilio on 23/11/15.
+ */
+public class BarraDeNombreFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.barra_de_nombre_fragment, container, false);
+    }
+}
---
+++ Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/EstadisticaPersonajeFragment.java
@@ -0,0 +1,27 @@
+package ar.duelodeleyendas.duelo_android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by exilio on 23/11/15.
+ */
+public class EstadisticaPersonajeFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+}
