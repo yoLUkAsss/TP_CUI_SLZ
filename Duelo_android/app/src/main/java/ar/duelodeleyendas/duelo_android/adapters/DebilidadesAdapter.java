81efe0b7e0d96b46605b21404a6da8da9446a0c9
ExiLio <sandoval.lucasj@gmail.com>
2015-11-28 03:07:05 -0300

    Parametrizando un poco los adapters...

--- Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/adapters/DebilidadesAdapter.java
+++ Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/adapters/DebilidadesAdapter.java
@@ -1,60 +1,16 @@
 package ar.duelodeleyendas.duelo_android.adapters;
 
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 import ar.duelodeleyendas.duelo_android.R;
+import java.util.List;
 
 /**
  * Created by exilio on 25/11/15.
  */
-public class DebilidadesAdapter extends ArrayAdapter<String>  {
+public class DebilidadesAdapter extends AdapterPropio  {
 
-    Context mContext;
-    LayoutInflater inflater;
-    private List<String> debilidadesAMostrar = null;
-    private ArrayList<String> arraylist;
 
-    public DebilidadesAdapter(Context context, List<String> debilidades) {
-        super(context, R.layout.row_layout, debilidades);
-        this.mContext = context;
-        this.debilidadesAMostrar = debilidades;
-        this.inflater = LayoutInflater.from(mContext);
-        this.arraylist = new ArrayList<String>();
-        this.arraylist.addAll(debilidades);
-
-    }
-
-    @Override
-    public int getCount() {
-        return debilidadesAMostrar.size();
-    }
-
-    @Override
-    public String getItem(int position) {
-        return debilidadesAMostrar.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        convertView = inflater.inflate(R.layout.row_debilidad_layout, null);
-
-        return convertView;
+    public DebilidadesAdapter(Context context, List<String> lista, int layoutALlenar, int idABuscar){
+        super(context,lista,layoutALlenar,idABuscar);
     }
 }
