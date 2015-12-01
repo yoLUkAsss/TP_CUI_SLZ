2f2ff67157e15d00e2adfcbe9f3f71ab7a3abab7
Ramshell <ramshellcinox@gmail.com>
2015-12-01 16:12:13 -0300

    some styles

--- Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/EstadisticaPersonajeFragment.java
+++ Duelo_android/app/src/main/java/ar/duelodeleyendas/duelo_android/EstadisticaPersonajeFragment.java
@@ -21,33 +21,34 @@ public class EstadisticaPersonajeFragment extends Fragment {
 
     public EstadisticaPersonajeFragment(){}
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             estadisticaDePersonaje = (EstadisticaDePersonaje) getArguments().get(ARG_ITEM_ID);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.actual_estadistica_fragment, container, false);
-
-        if (estadisticaDePersonaje != null) {
-            ((TextView) rootView.findViewById(R.id.mejor_ubicacion_personaje)).setText(estadisticaDePersonaje.getMejorUbicacion());
-            ((TextView) rootView.findViewById(R.id.puntaje_personaje)).setText(estadisticaDePersonaje.getPuntaje());
-        }
-
+        if (estadisticaDePersonaje != null) { llenarEstadisticas(rootView); }
         return rootView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void llenarEstadisticas(View rootView) {
+        ((TextView) rootView.findViewById(R.id.mejor_ubicacion_personaje)).setText(estadisticaDePersonaje.getMejorUbicacion());
+        ((TextView) rootView.findViewById(R.id.puntaje_personaje)).setText(estadisticaDePersonaje.getPuntaje());
+        ((TextView) rootView.findViewById(R.id.jugadas_personaje)).setText(estadisticaDePersonaje.getJugadas());
+        ((TextView) rootView.findViewById(R.id.ganadas_personaje)).setText(estadisticaDePersonaje.getGanadas());
+        ((TextView) rootView.findViewById(R.id.kills_personaje)).setText(estadisticaDePersonaje.getKills());
+        ((TextView) rootView.findViewById(R.id.deaths_personaje)).setText(estadisticaDePersonaje.getDeaths());
+        ((TextView) rootView.findViewById(R.id.assists_personaje)).setText(estadisticaDePersonaje.getAssists());
     }
 
 }
